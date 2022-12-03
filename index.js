33719e25a3948f7eb49f088d9dc32b65ae74133e
sramk87 <sramkissoon87@gmail.com>
2022-12-03 00:48:10 -0500

    questions array to index

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ const Manager = require('./lib/Manager');
 const generateHtml = require('./src/generateHtml');
 
 
+
 const questionsArr = [
     {
         type: 'input',
@@ -17,7 +18,7 @@ const questionsArr = [
             if (!nameInput) {
                 console.log("Please enter the manager's name");
                 return false;
-            }
+            } else
             {return true;}
         }
     },
@@ -29,7 +30,7 @@ const questionsArr = [
             if(!idInput) {
                 console.log("Please enter manager's Id");
                 return false;
-            }
+            } else
             {return true;}
         }
     },
@@ -41,7 +42,7 @@ const questionsArr = [
             if (!emailInput) {
                 console.log("Please enter your manager's email");
                 return false;
-            }
+            } else
             {return true;}
         }
     },
@@ -53,7 +54,7 @@ const questionsArr = [
         if (!officeNumInput) {
             console.log("Please enter the manager's office number");
             return false;
-        }
+        } else
         {return true;}
     }
     },
@@ -61,45 +62,177 @@ const questionsArr = [
 
 const emplQuestionaArr = [
     {
-        type: 'input',
-        name: 'employeesName',
-        message: 'What is the name of the employee?',
-        validate: (nameInput) => {
-            if (!nameInput) {
-                console.log("Please enter the employee's name");
-                return false;
-            }
-            {return true;}
-        }
-    },
-    {
-        type: 'input',
-        name: 'employeesId',
-        message: 'What is the Id of the employee?',
-        validate: (idInput) => {
-            if(!idInput) {
-                console.log("Please enter employee's Id");
-                return false;
-            }
-            {return true;}
-        }
-    },
-    {
-        type: 'input',
-        name: 'employeesEmail',
-        message: 'What is the email of the employee?',
-        validate: (emailInput) => {
-            if (!emailInput) {
-                console.log("Please enter your employee's email");
-                return false;
-            }
-            {return true;}
-        }
-    },
-    {
     type: 'list',
     name: 'emplRole',
     message: 'What is the role of the employee?',
     choices: ['Engineer', 'Intern'],
     },
+    {
+    type: 'input',
+    name: 'engineersName',
+    message: 'What is the name of the engineer?',
+    validate: (engNameInput) => {
+        if (!engNameInput) {
+            console.log("Please enter the engineer's name")
+            return false;
+        } else
+        {return true;}
+    }
+},
+when: ({ emplRole }) => {
+    if ( emplRole === 'Engineer') {
+        return true;
+    }
+    else {
+        return false;
+    }
+},
+{
+    type: 'input',
+    name: 'engineersEmail',
+    message: 'What is the email of the engineer?',
+    validate: (engEmailInput) => {
+        if (!engEmailInput) {
+            console.log("Please enter the engineer's email")
+            return false;
+        } else
+        {return true;}
+    }
+},
+when: ({ emplRole }) => {
+    if ( emplRole === 'Engineer') {
+        return true;
+    }
+    else {
+        return false;
+    }
+},
+{
+    type: 'input',
+    name: 'engineersId',
+    message: 'What is the Id of the engineer?',
+    validate: (engIdInput) => {
+        if (!engIdInput) {
+            console.log("Please enter the engineer's id")
+            return false;
+        } else
+        {return true;}
+    }
+},
+when: ({ emplRole }) => {
+    if ( emplRole === 'Engineer') {
+        return true;
+    }
+    else {
+        return false;
+    }
+},
+{
+    type: 'input',
+    name: 'engineersGithub',
+    message: 'What is the github of the engineer?',
+    validate: (engGitInput) => {
+        if (!engGitInput) {
+            console.log("Please enter the engineer's github")
+            return false;
+        } else
+        {return true;}
+    }
+},
+when: ({ emplRole }) => {
+    if ( emplRole === 'Engineer') {
+        return true;
+    }
+    else {
+     return false;
+    }
+},
+{
+    type: 'input',
+    name: 'internsName',
+    message: 'What is the name of the intern?',
+    validate: (intNameInput) => {
+        if (!intNameInput) {
+            console.log("Please enter the intern's name")
+            return false;
+        } else
+        {return true;}
+    }
+},
+when: ({ emplRole }) => {
+    if ( emplRole === 'Intern') {
+        return true;
+    }
+    else {
+        return false;
+    }
+},
+{
+    type: 'input',
+    name: 'internsEmail',
+    message: 'What is the email of the intern?',
+    validate: (intEmailInput) => {
+        if (!intEmailInput) {
+            console.log("Please enter the intern's email")
+            return false;
+        } else
+        {return true;}
+    }
+},
+when: ({ emplRole }) => {
+    if ( emplRole === 'Intern') {
+        return true;
+    }
+    else {
+        return false;
+    }
+},
+{
+    type: 'input',
+    name: 'internsId',
+    message: 'What is the id of the intern?',
+    validate: (intIdInput) => {
+        if (!intIdInput) {
+            console.log("Please enter the intern's id")
+            return false;
+        } else
+        {return true;}
+    }
+},
+when: ({ emplRole }) => {
+    if ( emplRole === 'Intern') {
+        return true;
+    }
+    else {
+        return false;
+    }
+},
+{
+    type: 'input',
+    name: 'internsSchool',
+    message: 'What is the school of the intern?',
+    validate: (intSchoolInput) => {
+        if (!intSchoolInput) {
+            console.log("Please enter the intern's school")
+            return false;
+        } else
+        {return true;}
+    }
+},
+when: ({ emplRole }) => {
+    if ( emplRole === 'Intern') {
+        return true;
+    }
+    else {
+        return false;
+    }
+},
+{
+    type: 'list',
+    name: 'teamComplete',
+    message: 'Is your team complete?',
+    choices: ['Yes', 'No'],
+    default: 'Yes'
+}
 ];
+
